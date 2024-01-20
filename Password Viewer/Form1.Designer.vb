2be73a6f6e85de52a2af83fae2eb4244d79cca98
nikazizi56 <116706573+nikazizi56@users.noreply.github.com>
2024-01-20 22:06:28 +0800

    Initial commit

---
+++ Password Viewer/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(208, 32)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Check"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "WiFi Profile List"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 34)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(190, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 107)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(190, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(208, 105)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Copy"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(295, 139)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "GG WIFI Password Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Password Viewer/Form1.vb
@@ -0,0 +1,129 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Run the netsh command and populate ListBox with profile names
+        PopulateProfileList()
+    End Sub
+
+    Sub PopulateProfileList()
+        ' Run the netsh command and capture the output
+        Dim output As String = RunNetshCommand("wlan show profiles")
+
+        ' Extract profile names using regular expression
+        Dim profileNames As List(Of String) = ExtractProfileNames(output)
+
+        ' Populate ListBox with profile names
+        ComboBox1.Items.AddRange(profileNames.ToArray())
+    End Sub
+
+    Function RunNetshCommand(command As String) As String
+        Dim processInfo As New ProcessStartInfo("netsh", command)
+        processInfo.RedirectStandardOutput = True
+        processInfo.UseShellExecute = False
+        processInfo.CreateNoWindow = True
+
+        Dim process As New Process()
+        process.StartInfo = processInfo
+        process.Start()
+
+        Dim output As String = process.StandardOutput.ReadToEnd()
+        process.WaitForExit()
+
+        Return output
+    End Function
+
+    Function ExtractProfileNames(output As String) As List(Of String)
+        Dim profileNames As New List(Of String)
+
+        ' Use a regular expression to match profile names
+        Dim regex As New Regex("All User Profile\s+:\s+(.+)")
+        Dim matches As MatchCollection = regex.Matches(output)
+
+        ' Extract profile names from matches
+        For Each match As Match In matches
+            If match.Groups.Count > 1 Then
+                Dim profileName As String = match.Groups(1).Value
+                profileNames.Add(profileName)
+            End If
+        Next
+
+        Return profileNames
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CheckKey()
+    End Sub
+
+    Sub CheckKey()
+        Try
+            ' Get the selected profile name from ComboBox
+            If ComboBox1.SelectedItem IsNot Nothing Then
+                Dim selectedProfile As String = ComboBox1.SelectedItem.ToString().Trim()
+                Dim output As String = RunNetshCommand("wlan show profiles """ & selectedProfile & """ key=clear")
+
+                ' Extract password using regular expression
+                Dim password As String = ExtractPassword(output)
+
+                ' Display the password in TextBox1
+                TextBox1.Text = password
+            Else
+                MessageBox.Show("Please select a profile before checking the key.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+    Function RunKeyCommand(command As String) As String
+        Dim processInfo As New ProcessStartInfo("netsh", command)
+        processInfo.RedirectStandardOutput = True
+        processInfo.UseShellExecute = False
+        processInfo.CreateNoWindow = True
+
+        Dim process As New Process()
+        process.StartInfo = processInfo
+        process.Start()
+
+        Dim output As String = process.StandardOutput.ReadToEnd()
+        process.WaitForExit()
+
+        Return output
+    End Function
+    Function ExtractPassword(output As String) As String
+        ' Use a regular expression to match the Key Content line
+        Dim regex As New Regex("Key Content\s+:\s+(.+)", RegexOptions.IgnoreCase)
+        Dim match As Match = regex.Match(output)
+
+        ' Check if there is a match
+        If match.Success Then
+            ' Check if the matched group contains the password
+            If match.Groups.Count > 1 Then
+                ' Return the password
+                Return match.Groups(1).Value.Trim()
+            Else
+                ' Return a message indicating that the password was found but the group is missing
+                Return "Password found, but group missing"
+            End If
+        Else
+            ' Return a message indicating that the password was not found
+            Return "Password not found"
+        End If
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Check if TextBox2 has text before copying to clipboard
+        If Not String.IsNullOrEmpty(TextBox1.Text) Then
+            ' Copy text to clipboard
+            Clipboard.SetText(TextBox1.Text)
+
+            ' Optionally, provide feedback to the user
+            MessageBox.Show("Password copied to clipboard!", "App Made by NikGG", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            ' Notify the user if TextBox1 is empty
+            MessageBox.Show("TextBox is empty. Nothing to copy.", "App Made by NikGG", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+End Class
